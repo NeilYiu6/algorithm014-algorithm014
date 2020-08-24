b40ed847ab2b41be6b68f8077943a5357ae6916a
classicneily <classicneily@outlook.com>
2020-08-24 12:49:30 +0800

    二叉树遍历补充

--- Week_02/144. Binary Tree Preorder Traversal.js
+++ Week_02/144. Binary Tree Preorder Traversal.js
@@ -10,57 +10,30 @@
 //    3
 
 // Output: [1,2,3]
-
-// Input: [1,2,6,3,5]
-//      1
-//     / \
-//    2   6
-//   / \ 
-//  3   5
-
-// Output: [3,5,2,6,1]
-
 // Follow up: Recursive solution is trivial, could you do it iteratively?
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+//   Definition for a binary tree node.
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
-// var preorderTraversal = function (root) {
-//     let res = []
-//     let recur = function (node) {
-//         if (!node) {
-//             return
-//         }
-//         res.push(node.val)
-//         recur(node.left)
-//         recur(node.right)
-//     }
-//     recur(root)
-//     return res
-// };
-
-// iterative
-// 风格和中序不统一
-// var preorderTraversal = function (root) {
-//     if (!root) {
-//         return []
-//     }
-//     let stack = [root], res = []
-//     while (stack.length) {
-//         let node = stack.pop()
-//         res.push(node.val)
-//         node.right && stack.push(node.right)
-//         node.left && stack.push(node.left)
-//     }
-//     return res
-// }
+var preorderTraversal = function (root) {
+    let res = []
+    let recur = function (node) {
+        if (!node) {
+            return
+        }
+        res.push(node.val)
+        recur(node.left)
+        recur(node.right)
+    }
+    recur(root)
+    return res
+};
 
 var preorderTraversal = function (root) {
     if (!root) {
@@ -69,14 +42,9 @@ var preorderTraversal = function (root) {
     let stack = [root], res = []
     while (stack.length) {
         let node = stack.pop()
-        if (node) {
-            node.right && stack.push(node.right)
-            node.left && stack.push(node.left)
-            stack.push(node)
-            stack.push(null)
-        } else {
-            res.push(stack.pop().val)
-        }
+        res.push(node.val)
+        node.right && stack.push(node.right)
+        node.left && stack.push(node.left)
     }
     return res
 }
